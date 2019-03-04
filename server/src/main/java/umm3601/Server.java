6d728fde0c1c99f9fb458529030a9e6010a3c104
JReuvers <reuve026@morris.umn.edu>
2019-03-04 17:09:43 -0600

    Fixes path naming for consistency

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -73,8 +73,8 @@ public class Server {
     //List todos, filtered using query parameters
 
     get("api/todos", todoRequestHandler::getTodos);
-    get("api/user/:id", todoRequestHandler::getTodoJSON);
-    get("api/users/new", todoRequestHandler::addNewTodo);
+    get("api/todos/:id", todoRequestHandler::getTodoJSON);
+    get("api/todos/new", todoRequestHandler::addNewTodo);
 
     // An example of throwing an unhandled exception so you can see how the
     // Java Spark debugger displays errors like this.
